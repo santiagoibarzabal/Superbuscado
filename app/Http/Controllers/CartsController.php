42d30a60f56169056d1f5f544eb847a6fa9e7d77
santiagoibarzabal <santiagoibarzabal91@gmail.com>
2019-11-04 14:31:29 -0300

    Feature: Agregué use App\Modelo a todos los controladores

--- app/Http/Controllers/CartsController.php
+++ app/Http/Controllers/CartsController.php
@@ -4,6 +4,8 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
+use App\Cart;
+
 class CartsController extends Controller
 {
     /**
--- app/Http/Controllers/StoresController.php
+++ app/Http/Controllers/StoresController.php
@@ -4,6 +4,8 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
+use App\Store;
+
 class StoresController extends Controller
 {
     /**
