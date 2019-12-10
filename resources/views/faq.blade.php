549e6ff34ea414d888f0357ecdea1714833fda8f
Sofía Lancuba <sofia.lancuba@gmail.com>
2019-12-09 21:39:12 -0300

    Feature: view faq

---
+++ resources/views/faq.blade.php
@@ -0,0 +1,164 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <!-- Meta -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <!-- Links -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <link href="https://fonts.googleapis.com/css?family=Lato:100,100i,300,300i,400,400i,700,700i,900i&display=swap" rel="stylesheet">
+
+    <link rel="stylesheet" href="css/faq_style.css">
+      <!--Titulo  -->
+    <title>Preguntas Frecuentes</title>
+  </head>
+  <body>
+   <!-- Barra de Navegacion -->
+   <nav class="navbar navbar-dark bg-success">
+     <a href="#"><img class="logo-navbar" src="imagenes/logo-superbuscado-white.png" alt=""></a>
+      <form class="form-inline">
+       <input class="form-control mr-sm-2" type="search" placeholder="Que dudas tenes?" aria-label="Search" id="search">
+        <button class="btn btn-outline-light btn-md mr-sm-3" type="submit">Buscar</button>
+      </form>
+    </nav>
+    <!-- FAQ -->
+    <h1> Preguntas Frecuentes </h1>
+    <div class="accordion" id="accordionExample">
+      <div class="card">
+        <div class="card-header" id="headingUno">
+          <h2 class="mb-0">
+            <button class="btn collapsed btn-success btn-lg btn-block" type="button" data-toggle="collapse" data-target="#collapseUno" aria-expanded="false" aria-controls="collapseUno">
+              Como me registro?
+            </button>
+          </h2>
+        </div>
+        <div id="collapseUno" class="collapse" aria-labelledby="headingUno" data-parent="#accordionExample">
+          <div class="card-body">
+            Escribí el nombre del producto que buscás en el casillero Producto. También podés hacer click en el ícono de las categorías.
+          </div>
+        </div>
+      </div>
+  <div class="card">
+    <div class="card-header" id="headingDos">
+      <h2 class="mb-0">
+        <button class="btn collapsed btn-success btn-lg btn-block" type="button" data-toggle="collapse" data-target="#collapseDos" aria-expanded="false" aria-controls="collapseDos">
+          Como busco un producto?
+        </button>
+      </h2>
+    </div>
+    <div id="collapseDos" class="collapse" aria-labelledby="headingDos" data-parent="#accordionExample">
+      <div class="card-body">
+        Escribí el nombre del producto que buscás en el casillero Producto. También podés hacer click en el ícono de las categorías.
+      </div>
+    </div>
+  </div>
+  <div class="card">
+    <div class="card-header" id="headingTres">
+      <h2 class="mb-0">
+        <button class="btn collapsed btn-success btn-lg btn-block" type="button" data-toggle="collapse" data-target="#collapseTres" aria-expanded="false" aria-controls="collapseTres">
+          Como establezco mi ubicacion?
+        </button>
+      </h2>
+    </div>
+    <div id="collapseTres" class="collapse" aria-labelledby="headingTres" data-parent="#accordionExample">
+      <div class="card-body">
+        Escribí el nombre del producto que buscás en el casillero Producto. También podés hacer click en el ícono de las categorías.
+      </div>
+    </div>
+  </div>
+  <div class="card">
+    <div class="card-header" id="headingCuatro">
+      <h2 class="mb-0">
+        <button class="btn collapsed btn-success btn-lg btn-block" type="button" data-toggle="collapse" data-target="#collapseCuatro" aria-expanded="false" aria-controls="collapseCuatro">
+          Puedo cambiar mi direccion?
+        </button>
+      </h2>
+    </div>
+    <div id="collapseCuatro" class="collapse" aria-labelledby="headingCuatro" data-parent="#accordionExample">
+      <div class="card-body">
+        Escribí el nombre del producto que buscás en el casillero Producto. También podés hacer click en el ícono de las categorías.
+      </div>
+    </div>
+  </div>
+  <div class="card">
+    <div class="card-header" id="headingCinco">
+      <h2 class="mb-0">
+        <button class="btn collapsed btn-success btn-lg btn-block" type="button" data-toggle="collapse" data-target="#collapseCinco" aria-expanded="false" aria-controls="collapseCinco">
+          Que comercios encuentro en SuperBuscado?
+        </button>
+      </h2>
+    </div>
+    <div id="collapseCinco" class="collapse" aria-labelledby="headingCinco" data-parent="#accordionExample">
+      <div class="card-body">
+        Escribí el nombre del producto que buscás en el casillero Producto. También podés hacer click en el ícono de las categorías.
+      </div>
+      <button class="btn collapsed btn-success btn-lg btn-block" type="button" data-toggle="collapse" data-target="#collapseCinco" aria-expanded="false" aria-controls="collapseCinco">
+    </div>
+  </div>
+  <div class="card">
+    <div class="card-header" id="headingSeis">
+      <h2 class="mb-0">
+        <button class="btn collapsed btn-success btn-lg btn-block" type="button" data-toggle="collapse" data-target="#collapseSeis" aria-expanded="false" aria-controls="collapseSeis">
+          Como armo mi lista de productos?
+        </button>
+      </h2>
+    </div>
+    <div id="collapseSeis" class="collapse" aria-labelledby="headingSeis" data-parent="#accordionExample">
+      <div class="card-body">
+        Escribí el nombre del producto que buscás en el casillero Producto. También podés hacer click en el ícono de las categorías.
+      </div>
+    </div>
+  </div>
+  <div class="card">
+    <div class="card-header" id="headingSiete">
+      <h2 class="mb-0">
+        <button class="btn collapsed btn-success btn-lg btn-block" type="button" data-toggle="collapse" data-target="#collapseSiete" aria-expanded="false" aria-controls="collapseSiete">
+          Como comparo los precios de distintos comercios?
+        </button>
+      </h2>
+    </div>
+    <div id="collapseSiete" class="collapse" aria-labelledby="headingSiete" data-parent="#accordionExample">
+      <div class="card-body">
+        Escribí el nombre del producto que buscás en el casillero Producto. También podés hacer click en el ícono de las categorías.
+      </div>
+    </div>
+  </div>
+  <div class="card">
+    <div class="card-header" id="headingOcho">
+      <h2 class="mb-0">
+        <button class="btn collapsed btn-success btn-lg btn-block" type="button" data-toggle="collapse" data-target="#collapseOcho" aria-expanded="false" aria-controls="collapseOcho">
+          Como confirmo la entrega?
+        </button>
+      </h2>
+    </div>
+    <div id="collapseOcho" class="collapse" aria-labelledby="headingOcho" data-parent="#accordionExample">
+      <div class="card-body">
+        Escribí el nombre del producto que buscás en el casillero Producto. También podés hacer click en el ícono de las categorías.
+      </div>
+    </div>
+  </div>
+  <div class="card">
+    <div class="card-header" id="headingNueve">
+      <h2 class="mb-0">
+        <button class="btn collapsed btn-success btn-lg btn-block" type="button" data-toggle="collapse" data-target="#collapseNueve" aria-expanded="false" aria-controls="collapseNueve">
+          No logramos cubrir todas tus dudas?
+        </button>
+      </h2>
+    </div>
+    <!-- Agregar Link hacia contacto -->
+    <div id="collapseNueve" class="collapse" aria-labelledby="headingNueve" data-parent="#accordionExample">
+      <div class="card-body">
+          Realizanos tu consulta @Contacto
+      </div>
+    </div>
+  </div>
+</div>
+
+
+    <!-- JQuery-->
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+  </body>
+</html>
