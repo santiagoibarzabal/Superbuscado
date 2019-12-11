4cc900cdf1eabf6abb800bc97b7dd320665153f4
santiagoibarzabal <santiagoibarzabal91@gmail.com>
2019-12-11 15:25:23 -0300

    Fix: Estilos de paginación en template bootstrap/laravel.

--- resources/views/vendor/pagination/bootstrap-4.blade.php
+++ resources/views/vendor/pagination/bootstrap-4.blade.php
@@ -1,13 +1,13 @@
 @if ($paginator->hasPages())
-    <ul class="pagination" role="navigation">
+    <ul class="col-12 pagination d-flex justify-content-center mt-4" role="navigation">
         {{-- Previous Page Link --}}
         @if ($paginator->onFirstPage())
             <li class="page-item disabled" aria-disabled="true" aria-label="@lang('pagination.previous')">
-                <span class="page-link" aria-hidden="true">&lsaquo;</span>
+                <span class="page-link text-success" aria-hidden="true">&lsaquo;</span>
             </li>
         @else
             <li class="page-item">
-                <a class="page-link" href="{{ $paginator->previousPageUrl() }}" rel="prev" aria-label="@lang('pagination.previous')">&lsaquo;</a>
+                <a class="page-link text-success" href="{{ $paginator->previousPageUrl() }}" rel="prev" aria-label="@lang('pagination.previous')">&lsaquo;</a>
             </li>
         @endif
 
@@ -15,16 +15,16 @@
         @foreach ($elements as $element)
             {{-- "Three Dots" Separator --}}
             @if (is_string($element))
-                <li class="page-item disabled" aria-disabled="true"><span class="page-link">{{ $element }}</span></li>
+                <li class="page-item disabled" aria-disabled="true"><span class="page-link text-success">{{ $element }}</span></li>
             @endif
 
             {{-- Array Of Links --}}
             @if (is_array($element))
                 @foreach ($element as $page => $url)
                     @if ($page == $paginator->currentPage())
-                        <li class="page-item active" aria-current="page"><span class="page-link">{{ $page }}</span></li>
+                        <li class="page-item active bg-success" aria-current="page"><span class="page-link bg-success border-success">{{ $page }}</span></li>
                     @else
-                        <li class="page-item"><a class="page-link" href="{{ $url }}">{{ $page }}</a></li>
+                        <li class="page-item"><a class="page-link text-success" href="{{ $url }}">{{ $page }}</a></li>
                     @endif
                 @endforeach
             @endif
@@ -33,11 +33,11 @@
         {{-- Next Page Link --}}
         @if ($paginator->hasMorePages())
             <li class="page-item">
-                <a class="page-link" href="{{ $paginator->nextPageUrl() }}" rel="next" aria-label="@lang('pagination.next')">&rsaquo;</a>
+                <a class="page-link text-success" href="{{ $paginator->nextPageUrl() }}" rel="next" aria-label="@lang('pagination.next')">&rsaquo;</a>
             </li>
         @else
             <li class="page-item disabled" aria-disabled="true" aria-label="@lang('pagination.next')">
-                <span class="page-link" aria-hidden="true">&rsaquo;</span>
+                <span class="page-link text-success" aria-hidden="true">&rsaquo;</span>
             </li>
         @endif
     </ul>
