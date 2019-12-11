5b6613630a6831764eaca859ca1d93c419a50aa2
Sofía Lancuba <sofia.lancuba@gmail.com>
2019-12-11 06:07:17 -0300

    Fix: query DB index

--- app/Http/Controllers/CartsController.php
+++ app/Http/Controllers/CartsController.php
@@ -26,88 +26,93 @@ class CartsController extends Controller
     public function index($id)
     {
 
+      // --------------------------  GEOLOCATION  --------------------------
+
       // $userAddress = auth()->user()->address;
       //
       // $geolocation = curl_init('https://maps.googleapis.com/maps/api/geocode/json?address=' . str_replace(' ', '+', $userAddress->address) . ',+' . str_replace(' ', '+', $userAddress->city) . ',+' . str_replace(' ', '+', $userAddress->province) . '&key='. env('GMAPS_KEY'));
       //
       // $json = curl_exec($geolocation);
-      // dd($json['geometry']);
+      // dd($json);
 
+      // --------------------------  QUERY SQL  --------------------------
 
-
-
-
-
-      // $url = 'https://maps.googleapis.com/maps/api/geocode/json?latlng=40.714224,-73.961452&key=' . env('GMAPS_KEY');
-
-      // Buscar todos los products de la listing que estén en stock y traer los stores con su market
-      // $listing = Listing::with(['products' => function ($products) {
-      //   return $products->with('stocks')->whereHas('stocks', function ($stocks) {
-      //     return $stocks->with('store', 'store.market')->where('quantity', '>=', 0);
-      //   });
-      // }])->find($id);
-
-      $markets = Market::all();
-
-      // TRAER TODOS LOS PRODUCTOS DE LA LISTA AGRUPADOS POR STORE
-      // -------------------------------------------------------------
-
-      // $listing = Listing::with(['products' => function ($product) {
-      //    return $product;
-      //  }])->find($id);
+      // select
+      // sum(stk.list_price*lspr.quantity) as total_price,
+      // sum(lspr.quantity),
+      // count(lspr.listing_id),
+      // m.id MarketId,
+      // st.id StoreId,
+      // st.zip_code StoreZip_code,
+      // us.id UserId,
+      // ad.zip_code AddressUser,
+      // lspr.listing_id ListingId,
+      // lspr.product_id ProductId,
+      // lspr.quantity QuantityProducts,
+      // stk.list_price ListPrice,
+      // stk.quantity QuantityStock
       //
-      // dd($listing);
+      // from stocks as stk
+      // join stores as st on stk.store_id = st.id
+      // join markets as m on st.market_id = m.id
+      // join listing_product as lspr on stk.product_id = lspr.product_id
+      // join listings as ls on lspr.listing_id = ls.id
+      // join users as us on ls.user_id = us.id
+      // join addresses as ad on us.id = ad.user_id
+      //
+      // WHERE (st.zip_code = ad.zip_code)
+      // GROUP by st.id, lspr.listing_id
+      // ORDER BY total_price
 
 
+      $list = Listing::find($id);
+
+      $query = \DB::table('stocks')
+                  ->select(
+                  // \DB::raw('COUNT(listing_product.listing_id) as count'),
+                  \DB::raw('SUM(stocks.list_price*listing_product.quantity) as total_price'),
+                  \DB::raw('SUM(listing_product.quantity) as total_quantity'),
+                  'stores.address as store_address',
+                  'markets.name as market_name',
+                  'markets.logo as logo',
+                  'stores.zip_code as store_zip_code', 'listing_product.listing_id',
+                  'listing_product.product_id',
+                  'listing_product.quantity as product_quantity',
+                  'users.id as user_id',
+                  'addresses.zip_code as address_zip_code',
+                  'stocks.list_price',
+                  'stocks.quantity as stoks_quantity')
+
+                  ->join('stores', 'stocks.store_id', '=', 'stores.id')
+                  ->join('markets', 'stores.market_id', '=', 'markets.id')
+                  ->join('listing_product', 'stocks.product_id', '=', 'listing_product.product_id')
+                  ->join('listings', 'listing_product.listing_id', '=', 'listings.id')
+                  ->join('users', 'listings.user_id', '=', 'users.id')
+                  ->join('addresses', 'users.id', '=', 'addresses.user_id')
+                  ->where('stores.zip_code', auth()->user()->address->zip_code)
+                  ->groupBy('listings.id', 'stores.id')
+                  ->orderBy('total_price')
+
+                  // ->offset(20000)
+                  // ->limit(10)
+                  ->get();
+
+
+
+      // --------------------------  INTENTO DE QUERY  --------------------------
 
       // $listing = Listing::with(['products' => function ($product) {
       //   return $product->with(['stocks' => function ($stock){
-      //     return $stock->with(['store' => function($store) {
+      //     return $stock->where('quantity', '>', 0)->with(['store' => function($store){
       //       return $store->where('zip_code', auth()->user()->address->zip_code);
-      //     }])->get()->groupBy('store_id');
+      //     }]);
       //   }]);
       // }])->find($id);
 
-      // dd($listing->products[0]->stocks);
-
-
-
-
-
-      // $query = \DB::getPdo()->query('select m.id MarketId, s.id StoreId, st.product_id ProductId, st.quantity InStock, sum(st.quantity) Units, st.list_price ListPrice from `markets` as m join stores as s on s.market_id = m.id join stocks as st on st.store_id = s.id where st.product_id in (259,260,272) group by st.store_id order by StoreId, ProductId')->fetch();
-      //
-      // dd($query);
-
-
-
-
-      $listing = Listing::with(['products' => function ($product) {
-        return $product->with(['stocks' => function ($stock){
-          return $stock->where('quantity', '>', 0)->with(['store' => function($store){
-            return $store->where('zip_code', auth()->user()->address->zip_code);
-          }]);
-        }]);
-      }])->find($id);
-
-      // dd($listing->products->groupBy('stock')->toArray());
-
-      // dd($listing->products[0]->stocks[0]->store);
-
-      // $nears = $listing->products->filter(function ($p) {
-      //   return $p->stocks->filter(function ($s) {
-      //     return $s->store->zip_code == auth()->user()->address->zip_code;
-      //   });
-      // });
-
-      // REVISAR----------------
-      // $productsInList = $listing->products->pluck('id', 'name');
-
-      // dd($productsInList);
 
       return view('carts.index', [
-        'listing' => $listing,
-        // 'nears' => $nears,
-        'markets' => $markets
+        'query' => $query,
+        'list' => $list
       ]);
     }
 
